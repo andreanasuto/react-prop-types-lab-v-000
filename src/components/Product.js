5f9cd46b170e1c3a48ff7e0638d9e4ceb627505e
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-19 20:51:39 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,59 @@
 // Code Product Component Here
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export class Product extends React.Component {
+  render () {
+    return (
+      <div className="product">
+        {this.props.name}
+        {this.props.producer}
+        {this.props.hasWatermark}
+        {this.props.color}
+        {this.props.weight}
+      </div>
+    )
+  }
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell', 'salmon']).isRequired,
+  weight: function(props, propName, componentName) {
+
+
+    if ((props[propName] > 300 || props[propName] < 80)) {
+      return new Error (
+        'validation failed 2'
+      )
+    }
+
+    if (!(typeof props[propName] === 'number')) {
+      return new Error (
+        'ciao'
+      )
+    }
+
+    if (!(propName in props)) {
+      return new Error (
+        'validation a'
+      )
+    }
+
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false,
+}
+
+export default Product;
+
+
+//  `name`: a string — required
+//  `producer`: a string — optional
+//  `hasWatermark`: a boolean — optional, defaults to false
+//  `color`: a string — required, can only be `'white'`, `'eggshell-white'` or `'salmon'`
+//  `weight`: a number — required, ranges between 80 and 300
